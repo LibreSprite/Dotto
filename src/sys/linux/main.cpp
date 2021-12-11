aa46f5f7ebefd157a23e616731849a6b79f173fe
Felipe Manga <lufeboma@gmail.com>
2021-12-11 19:55:17 +0000

    Initialize logger on Linux target

--- src/sys/linux/main.cpp
+++ src/sys/linux/main.cpp
@@ -2,5 +2,13 @@
 // This file is released under the terms of the MIT license.
 // Read LICENSE.txt for more information.
 
+#include <log/Log.hpp>
+
 int main(int argc, const char* argv[]) {
+    inject<Log> log{"stdout"};
+    log->setGlobal();
+    log->setLevel(Log::Level::VERBOSE); // TODO: Configure using args
+    Log::write(Log::Level::VERBOSE, "Logger initialized");
+
+    return 0;
 }
