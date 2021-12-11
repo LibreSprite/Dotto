7b43d49fd3112315abda46bdbef8b553d251b56f
Felipe Manga <lufeboma@gmail.com>
2021-12-11 18:45:24 +0000

    Added initial Linux main

---
+++ src/sys/linux/main.cpp
@@ -0,0 +1,6 @@
+// Copyright (c) 2021 LibreSprite Authors (cf. AUTHORS.md)
+// This file is released under the terms of the MIT license.
+// Read LICENSE.txt for more information.
+
+int main(int argc, const char* argv[]) {
+}
