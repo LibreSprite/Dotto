bb378b35d66175c62f612b522eac610131e91289
Felipe Manga <lufeboma@gmail.com>
2021-12-12 02:31:45 +0000

    Made Value const-correct

--- src/common/Value.hpp
+++ src/common/Value.hpp
@@ -10,19 +10,35 @@ class Value {
     std::any value;
 
 public:
+    template <typename Type>
+    Value(const Type& v) : value(v) {}
+
+    Value(const Value& v) : value(v.value) {}
+
+    Value(Value&& v) : value(std::move(v.value)) {}
+
     template<typename Type>
-    bool has() {
+    bool has() const {
         return value.has_value() && value.type() == typeid(Type);
     }
 
     template<typename Type>
-    operator Type () {
-        return has<Type>() ? std::any_cast<Type>() : Type{};
+    operator Type () const {
+        return has<Type>() ? std::any_cast<Type>(value) : Type{};
+    }
+
+    template<typename Type>
+    Type get() const {
+        return std::any_cast<Type>(value);
     }
 
     template<typename Type>
     Value& operator = (const Type& v) {
         value = v;
         return *this;
+    }
+
+    const char* typeName() const {
+        return value.type().name();
     }
 };
