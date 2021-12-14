398d7a58cfefb7f71fe5e802a857e17370c303d0
Felipe Manga <lufeboma@gmail.com>
2021-12-14 22:58:23 +0000

    Added helper methods for finding a Color in a Palette

--- src/doc/Palette.hpp
+++ src/doc/Palette.hpp
@@ -6,3 +6,28 @@
 #include <common/Color.hpp>
 
 using Palette = Vector<Color>;
+
+inline U32 findClosestColorIndex(const Palette& palette, const Color& color) {
+    if (palette.empty())
+        return color.r;
+    U32 closestIndex = 0;
+    auto closestDistance = palette[0].distanceSquared(color);
+    if (!closestDistance)
+        return closestIndex;
+    for (U32 index = 1, size = palette.size(); index < size; ++index) {
+        auto distance = palette[index].distanceSquared(color);
+        if (distance < closestDistance) {
+            if (!distance)
+                return index;
+            closestIndex = index;
+            closestDistance = distance;
+        }
+    }
+    return closestIndex;
+}
+
+inline Color findClosestColor(const Palette& palette, const Color& color) {
+    if (palette.empty())
+        return {color.r, color.r, color.r, 255};
+    return palette[findClosestColorIndex(palette, color)];
+}
