ef24fa34ddaa1993730b2c6dfcdc692d0901e362
Felipe Manga <lufeboma@gmail.com>
2021-12-16 21:04:02 +0000

    Use FS parser to read settings.ini

--- src/common/Config.cpp
+++ src/common/Config.cpp
@@ -11,14 +11,12 @@ public:
     inject<FileSystem> fs;
 
     bool boot() override {
-        auto file = fs->find("%userdata/settings.ini")->get<File>();
-        if (!file || !file->open()) {
-            file = fs->find("%appdata/settings.ini")->get<File>();
-        }
-        if (!file || !file->open()) {
+        properties = fs->find("%userdata/settings.ini")->parse();
+        if (!properties)
+            properties = fs->find("%appdata/settings.ini")->parse();
+        if (!properties) {
             Log::write(Log::Level::ERROR, "Could not open settings file. Reinstalling may fix this problem.");
-        } else if (auto parser = inject<Parser>{"ini"}) {
-            properties = parser->parseFile(file).get<std::shared_ptr<PropertySet>>();
+            return false;
         }
         return true;
     }
