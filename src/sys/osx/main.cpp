597609b59c5518616f22b840731574357df09192
Felipe Manga <lufeboma@gmail.com>
2021-12-19 02:40:33 +0000

    Untested OSX support

---
+++ src/sys/osx/main.cpp
@@ -0,0 +1,24 @@
+// Copyright (c) 2021 LibreSprite Authors (cf. AUTHORS.md)
+// This file is released under the terms of the MIT license.
+// Read LICENSE.txt for more information.
+
+#ifdef __APPLE__
+
+#include <app/App.hpp>
+#include <common/System.hpp>
+#include <log/Log.hpp>
+
+int main(int argc, const char* argv[]) {
+    Log::setDefault("stdout");
+    System::setDefault("sdl2");
+
+    inject<App> app{"dotto"};
+    app->boot(argc, argv);
+
+    Log::write(Log::Level::VERBOSE, "Running Dotto!");
+    while(app->run());
+
+    return 0;
+}
+
+#endif
---
+++ src/sys/osx/osxfs.mm
@@ -0,0 +1,86 @@
+// Copyright (c) 2021 LibreSprite Authors (cf. AUTHORS.md)
+// This file is released under the terms of the MIT license.
+// Read LICENSE.txt for more information.
+
+#ifdef __APPLE__
+
+#include <limits.h>
+#include <unistd.h>
+
+#include <common/String.hpp>
+#include <fs/FileSystem.hpp>
+#include <fs/Folder.hpp>
+#include <fs/StdFile.hpp>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+static auto fontPaths = std::array{
+    "/System/Library/Fonts/",
+    "/Library/Fonts",
+    "~/Library/Fonts"
+};
+
+class MacOSRootDir : public RootFolder {
+public:
+    bool boot() {
+        String appdir, home, config;
+        separator = "/";
+
+
+        {
+            home = std::getenv("HOME") ?: "";
+            if (home.empty())
+                home = "~";
+            mount("%userhome", "dir", home);
+        }
+
+        {
+
+            NSArray* dirs = NSSearchPathForDirectoriesInDomains(NSApplicationSupportDirectory, NSUserDomainMask, YES);
+            if (dirs) {
+                NSString* dir = [dirs firstObject];
+                if (dir)
+                    config = [dir UTF8String];
+            }
+
+            if (config.empty())
+                config = home + separator + ".config";
+            config += separator + "dotto";
+            mount("%userdata", "dir", config);
+        }
+
+        {
+            String exePath;
+            exePath.resize(MAXPATHLEN);
+            uint32_t size = path.size();
+            while (_NSGetExecutablePath(&exePath[0], &size) == -1)
+                exePath.resize(size);
+            auto parts = split(exePath, separator);
+            parts.pop_back();
+            appdir = join(parts, separator);
+            mount("%appdir", "dir", appdir);
+            mount("%appdata", "dir", appdir + "/../Resources/data");
+        }
+
+        {
+            mount("%fonts", "dir", "");
+            if (auto fonts = std::static_pointer_cast<Folder>(getChild("%fonts", ""))) {
+                U32 index = 0;
+                for (auto& path : fontPaths) {
+                    struct stat buf;
+                    if (::stat(path, &buf) == 0 && S_ISDIR(buf.st_mode)) {
+                        fonts->mount(std::to_string(index++), "dir", String(path));
+                    }
+                }
+            }
+        }
+        return true;
+    }
+};
+
+static FSEntity::Shared<MacOSRootDir> lrd{"rootDir"};
+static FileSystem::Shared<FileSystem> fs{"new"};
+static File::Shared<StdFile> stdFile{"std"};
+static File::Shared<Folder> stdDir{"dir"};
+
+#endif
