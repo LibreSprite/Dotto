1276209cab4671a7e14402b47e3a80c86e32218b
Felipe Manga <lufeboma@gmail.com>
2021-12-26 01:06:32 +0000

    Implemented configurable Keyboard shortcut support

--- src/gui/controllers/MainWindow.cpp
+++ src/gui/controllers/MainWindow.cpp
@@ -12,22 +12,6 @@ class MainWindow : public ui::Controller {
 public:
     void attach() override {
         Tool::boot();
-        node()->addEventListener<ui::KeyDown, ui::KeyUp>(this);
-    }
-
-    void eventHandler(const ui::KeyDown& event) {
-        if (event.keycode == 'b') {
-            Tool::active = Tool::instances["pencil"];
-        } else if (event.keycode == 'f') {
-            Tool::active = Tool::instances["bucket"];
-        }
-        if (auto tool = Tool::active.lock())
-            logV("Active tool: ", tool->getName());
-        else
-            logV("Event keycode: ", event.keycode);
-    }
-
-    void eventHandler(const ui::KeyUp& event) {
     }
 };
 
---
+++ src/gui/controllers/Shortcut.cpp
@@ -0,0 +1,121 @@
+// Copyright (c) 2021 LibreSprite Authors (cf. AUTHORS.md)
+// This file is released under the terms of the MIT license.
+// Read LICENSE.txt for more information.
+
+#include "cmd/Command.hpp"
+#include "common/PropertySet.hpp"
+#include "common/String.hpp"
+#include <common/Config.hpp>
+#include <common/Messages.hpp>
+#include <common/PubSub.hpp>
+#include <cstdint>
+#include <fs/FileSystem.hpp>
+#include <gui/Controller.hpp>
+#include <gui/Events.hpp>
+#include <gui/Node.hpp>
+
+class Shortcut : public ui::Controller {
+    inject<Config> config;
+
+    class KeyEntry;
+    using KeyMap = HashMap<String, std::shared_ptr<KeyEntry>>;
+
+    class KeyEntry {
+        std::unique_ptr<KeyMap> childMap;
+        std::function<void()> action;
+
+    public:
+        KeyMap* getChildMap(Shortcut* shortcut) {
+            if (!childMap) {
+                childMap = std::make_unique<KeyMap>();
+                action = [this, shortcut]{
+                    auto keyCode = std::to_string(shortcut->lastKeyDown->keycode);
+                    auto it = childMap->find(keyCode);
+                    if (it == childMap->end())
+                        logI("KeyCode: ", keyCode);
+                    shortcut->currentMap = (it != childMap->end()) ? it->second.get() : &shortcut->defaultMap;
+                    if (!shortcut->currentMap->childMap || shortcut->currentMap->childMap->empty()) {
+                        shortcut->currentMap->activate();
+                    }
+                };
+            }
+            return childMap.get();
+        }
+
+        void setAction(std::function<void()>&& action) {
+            this->action = std::move(action);
+        }
+
+        void activate() {
+            if (action) {
+                action();
+            }
+        }
+    };
+
+    KeyEntry defaultMap;
+    KeyEntry* currentMap = &defaultMap;
+    const ui::KeyDown *lastKeyDown = nullptr;
+
+    void attach(ui::Node* node) {
+        if (!node)
+            return;
+        // attach(node->getParent());
+        auto& id = *node->id;
+        if (id.empty())
+            return;
+        auto containerProperties = config->properties->get<std::shared_ptr<PropertySet>>(id);
+        if (!containerProperties)
+            return;
+        auto bindings = containerProperties->get<std::shared_ptr<PropertySet>>("keybindings");
+        if (!bindings)
+            return;
+        for (auto& entry : bindings->getMap()) {
+            std::shared_ptr<PropertySet> commandList = *entry.second;
+            if (!commandList)
+                continue;
+            auto chords = split(entry.first, "+");
+            currentMap = &defaultMap;
+            for (auto& chord : chords) {
+                auto& map = *currentMap->getChildMap(this);
+                auto& ptr = map[trim(chord)];
+                if (!ptr)
+                    ptr = std::make_shared<KeyEntry>();
+                currentMap = ptr.get();
+            }
+            currentMap->setAction([=]{
+                currentMap = &defaultMap;
+                std::size_t size = commandList->getMap().size();
+                for (std::size_t i = 0; i < size; ++i) {
+                    auto properties = commandList->get<std::shared_ptr<PropertySet>>(std::to_string(i));
+                    if (!properties)
+                        continue;
+                    auto command = tolower(properties->get<String>("name"));
+                    if (command.empty())
+                        continue;
+                    if (auto instance = inject<Command>{command}) {
+                        instance->load(*properties);
+                        instance->run();
+                    }
+                }
+            });
+        }
+        currentMap = &defaultMap;
+    }
+
+public:
+    void attach() override {
+        node()->addEventListener<ui::KeyUp, ui::KeyDown>(this);
+        attach(node());
+    }
+
+    void eventHandler(const ui::KeyDown& event) {
+        lastKeyDown = &event;
+        currentMap->activate();
+    }
+
+    void eventHandler(const ui::KeyUp& event) {
+    }
+};
+
+static ui::Controller::Shared<Shortcut> shortcut{"shortcut"};
