2bbadbef65b912d0a3daac68accdc757e1946579
Felipe Manga <lufeboma@gmail.com>
2021-12-26 04:33:24 +0000

    Allow scripts to call arbitrary commands

--- src/script/api/AppScriptObject.cpp
+++ src/script/api/AppScriptObject.cpp
@@ -4,7 +4,10 @@
 
 #include <sstream>
 
+#include <cmd/Command.hpp>
+#include <common/PropertySet.hpp>
 #include <script/Engine.hpp>
+#include <script/Function.hpp>
 #include <script/ScriptObject.hpp>
 
 class AppScriptObject : public script::ScriptObject {
@@ -14,6 +17,19 @@ public:
     AppScriptObject() {
         target = getEngine().toValue(inject<script::ScriptTarget>{}->target);
         addProperty("target", [this]{return target;});
+        addFunction("command", [](const String& name){
+            inject<Command> command{name};
+            if (!command)
+                return false;
+            PropertySet properties;
+            auto& args = script::Function::varArgs();
+            for (std::size_t i = 1, size = args.size(); i + 1 < size; i += 2) {
+                properties.set(args[i], args[i + 1].str());
+            }
+            command->load(properties);
+            command->run();
+            return true;
+        });
         makeGlobal("app");
     }
 
