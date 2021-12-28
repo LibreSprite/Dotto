10a420658f1298b7299e2e6497823140e0bb22be
Felipe Manga <lufeboma@gmail.com>
2021-12-28 19:10:28 +0000

    Implemented Paint command for applying a Color onto a Cell

---
+++ src/cmd/Paint.cpp
@@ -0,0 +1,67 @@
+// Copyright (c) 2021 LibreSprite Authors (cf. AUTHORS.md)
+// This file is released under the terms of the MIT license.
+// Read LICENSE.txt for more information.
+
+#include <cmd/Command.hpp>
+#include <common/Messages.hpp>
+#include <common/PubSub.hpp>
+#include <doc/Cell.hpp>
+#include <doc/Selection.hpp>
+#include <log/Log.hpp>
+#include <tools/Tool.hpp>
+
+class Paint : public Command {
+    Property<std::shared_ptr<Selection>> selection{this, "selection"};
+    Property<Color> color{this, "color", Tool::color.toString()};
+    inject<Cell> cell{"activecell"};
+    Vector<U32> undoData;
+
+public:
+    void run() override {
+        auto selection = this->selection->get();
+        auto& color = *this->color;
+        if (!selection || !color.a) {
+            logI("bailing", !selection, !color.a);
+            return;
+        }
+
+        auto maskRect = selection->getBounds();
+        if (maskRect.empty())
+            return;
+
+        F32 alpha = color.a / 255.0f / 255.0f;
+        auto surface = cell->getComposite();
+        undoData = selection->read(surface);
+        auto& mask = selection->getData();
+        auto commonRect = maskRect;
+        auto surfaceData = surface->data();
+        auto maskStride = maskRect.width;
+        auto surfaceStride = surface->width();
+        Color tmp;
+        commonRect.intersect({0, 0, surface->width(), surface->height()});
+
+        U32 maskOffsetY = commonRect.y > 0 ? 0 : -commonRect.y;
+        U32 maskOffsetX = commonRect.x > 0 ? 0 : -commonRect.x;
+        U32 surfaceOffsetY = commonRect.y > 0 ? commonRect.y : 0;
+        U32 surfaceOffsetX = commonRect.x > 0 ? commonRect.x : 0;
+
+        for (U32 y = 0; y < commonRect.height; ++y) {
+            U32 maskIndex = (y + maskOffsetY) * maskStride + maskOffsetX;
+            U32 surfaceIndex = (y + surfaceOffsetY) * surfaceStride + surfaceOffsetX;
+            for (U32 x = 0; x < commonRect.width; ++x, ++maskIndex, ++surfaceIndex) {
+                auto amount = mask[maskIndex] * alpha;
+                tmp.fromU32(surfaceData[surfaceIndex]);
+                tmp.r = tmp.r * (1 - amount) + color.r * amount;
+                tmp.g = tmp.g * (1 - amount) + color.g * amount;
+                tmp.b = tmp.b * (1 - amount) + color.b * amount;
+                tmp.a = tmp.a + amount * (255 - tmp.a);
+                surfaceData[surfaceIndex] = tmp.toU32();
+            }
+        }
+
+        surface->setDirty();
+        selection->clear(); // temporary
+    }
+};
+
+static Command::Shared<Paint> cmd{"paint"};
