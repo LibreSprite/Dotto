72e14999295a0d5239c9f06726188c7eff771510
Felipe Manga <lufeboma@gmail.com>
2021-12-28 19:11:37 +0000

    Spray now uses Paint command instead of direct surface manipulation

--- data/autorun/100-tool-spray.js
+++ data/autorun/100-tool-spray.js
@@ -1,8 +1,42 @@
 app.addTool("spray");
+var selection;
+
+function len(x, y) {
+    return x*x + y*y;
+}
 
 function onEvent(name) {
-    if (name == "toolupdate")
-        app.target.surface.setPixel(app.target.lastX + Math.random() * 10 - 5,
-                                    app.target.lastY + Math.random() * 10 - 5,
-                                    app.target.color);
+    (({
+        init:function(){},
+
+        toolstart:function(){
+            app.release(selection);
+            selection = app.newSelection();
+            app.hold(selection);
+            onEvent("toolupdate");
+        },
+
+        toolupdate:function(){
+            var scatter = 6;
+            var cx = app.target.lastX + Math.random() * (scatter*2) - scatter;
+            var cy = app.target.lastY + Math.random() * (scatter*2) - scatter;
+            var size = 5 - (len(cx - app.target.lastX, cy - app.target.lastY) / (scatter));
+            var normalize = 255 / (size*size*1.44);
+            for (var y = cy - size; y < cy + size; ++y) {
+                for (var x = cx - size; x < cx + size; ++x) {
+                    var v = 255 - ((x-cx)*(x-cx)+(y-cy)*(y-cy)) * normalize;
+                    if (v > 0) selection.add(x, y, v);
+                }
+            }
+            app.command("paint", "selection", selection)
+        },
+
+        toolend:function(){
+            app.command("paint", "selection", selection)
+            selection = app.release(selection);
+        }
+
+    })[name] || (function(){
+        console.log("Unknown event: " + name);
+    }))();
 }
