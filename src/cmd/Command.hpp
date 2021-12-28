1926de37fae247369cb1123ccb441fdb2cbe2a78
Felipe Manga <lufeboma@gmail.com>
2021-12-28 00:55:45 +0000

    Associate commands with the currently active document

--- src/cmd/Command.hpp
+++ src/cmd/Command.hpp
@@ -6,10 +6,20 @@
 
 #include <common/inject.hpp>
 #include <common/PropertySet.hpp>
+#include <doc/Document.hpp>
 
 class Command : public Injectable<Command>,
                 public Model,
                 public std::enable_shared_from_this<Command> {
+    std::weak_ptr<Document> weakDoc;
+
+protected:
+    Command() {
+        weakDoc = inject<Document>{"activedocument"}.shared();
+    }
+
 public:
+    std::shared_ptr<Document> doc() {return weakDoc.lock();}
+
     virtual void run() = 0;
 };
