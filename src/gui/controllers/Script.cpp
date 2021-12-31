46aadef84b0b1a56499640c680e117599150e037
Felipe Manga <lufeboma@gmail.com>
2021-12-31 20:53:17 +0000

    Allow having scripts inlined in the xml

--- src/gui/controllers/Script.cpp
+++ src/gui/controllers/Script.cpp
@@ -15,14 +15,38 @@
 class ScriptController : public ui::Controller {
 public:
     std::shared_ptr<script::Engine> engine;
+    Property<bool> run{this, "run", false};
     Property<String> script{this, "script", "", &ScriptController::loadScript};
+    Property<String> type{this, "type", ""};
+    Property<String> text{this, "text", "", &ScriptController::loadScript};
 
     void loadScript() {
-        engine.reset();
+        if (!node() || !*run)
+            return;
+        *run = false;
+
         if (!script->empty()) {
             script::ScriptTarget target{node()->shared_from_this()};
             engine = FileSystem::parse(script);
+            return;
         }
+
+        if (type->empty()) {
+            engine.reset();
+            return;
+        }
+
+        script::Engine::PushDefault oldEngine;
+        script::Engine::setDefault(type, {type});
+        script::ScriptTarget target{node()->shared_from_this()};
+        engine = inject<script::Engine>{};
+        if (engine)
+            engine->eval(text);
+    }
+
+    void attach() override {
+        *run = true;
+        loadScript();
     }
 };
 
