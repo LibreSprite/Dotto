22a0fc790f3376604b2b1c507ae192d5b6bce242
Felipe Manga <lufeboma@gmail.com>
2022-01-08 02:48:08 +0000

    Improve input caret

--- src/gui/controllers/Input.cpp
+++ src/gui/controllers/Input.cpp
@@ -16,7 +16,6 @@ class Input : public ui::Controller {
 
 public:
     std::size_t cursorPosition = 0;
-    Property<String> value{this, "value", ""};
     Property<String> text{this, "text", ""};
     Property<String> spanId{this, "span", "value"};
     std::shared_ptr<ui::Node> span;
@@ -26,24 +25,48 @@ public:
     }
 
     void attach() override {
-        node()->addEventListener<ui::Focus, ui::Blur, ui::KeyDown, ui::KeyUp>(this);
+        node()->addEventListener<ui::MouseDown, ui::KeyDown, ui::KeyUp>(this);
         span = node()->findChildById(spanId);
     }
 
-    void eventHandler(const ui::Focus&) {
-        cursorPosition = value->size();
-        String text = this->value;
-        text.insert(cursorPosition, "∣");
-        node()->set("text", text);
+    void eventHandler(const ui::MouseDown& event) {
+        if (!span)
+            return;
+
+        auto advanceValue = span->get("text-advance");
+        if (!advanceValue || !advanceValue->has<Vector<S32>>())
+            return;
+
+        auto advance = advanceValue->get<Vector<S32>>();
+        S32 x = event.globalX - span->globalRect.x;
+        S32 cursorX = 0;
+        S32 nextCursorX = 0;
+        cursorPosition = 0;
+        for (U32 i = 0; i < advance.size(); ++i) {
+            nextCursorX += advance[i];
+            if (std::abs(x - cursorX) > std::abs(x - nextCursorX)) {
+                cursorPosition = i;
+                cursorX = nextCursorX;
+            }
+        }
+
+        span->set("text", ""); // force redraw to get rid of caret
+        span->set("text", *text);
+        auto surfaceValue = span->get("surface");
+        if (surfaceValue && surfaceValue->has<std::shared_ptr<Surface>>()) {
+            drawCaret(surfaceValue->get<std::shared_ptr<Surface>>(), cursorX);
+        }
     }
 
-    void eventHandler(const ui::Blur&) {
-        node()->set("text", *value);
+    void drawCaret(std::shared_ptr<Surface> surface, S32 cursorX) {
+        for (S32 y = 0; y < surface->height(); ++y) {
+            surface->setPixel(cursorX, y, Color{0,0,0});
+        }
     }
 
     void eventHandler(const ui::KeyDown& event) {
         String keyName = event.keyname;
-        String text = this->value;
+        String text = this->text;
         cursorPosition = std::min(text.size(), cursorPosition);
         if (keyName == "BACKSPACE") {
             if (!cursorPosition)
@@ -61,9 +84,27 @@ public:
         } else {
             return;
         }
-        node()->set("value", text);
-        text.insert(cursorPosition, "∣");
+
+        if (span)
+            span->set("text", ""); // force redraw to get rid of caret
         node()->set("text", text);
+        node()->set("value", text);
+
+        if (span) {
+            auto advanceValue = span->get("text-advance");
+            auto surfaceValue = span->get("surface");
+            if (advanceValue && advanceValue->has<Vector<S32>>() &&
+                surfaceValue && surfaceValue->has<std::shared_ptr<Surface>>()) {
+                auto advance = advanceValue->get<Vector<S32>>();
+                auto surface = surfaceValue->get<std::shared_ptr<Surface>>();
+                auto end = std::min(cursorPosition, advance.size());
+                S32 cursorX = 0;
+                for (std::size_t i = 0; i < end; ++i) {
+                    cursorX += advance[i];
+                }
+                drawCaret(surface, cursorX);
+            }
+        }
     }
 
     void eventHandler(const ui::KeyUp& event) {
