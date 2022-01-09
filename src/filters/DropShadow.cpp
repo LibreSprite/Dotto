283ec28aba3161568dcf1ba56a5fe029c93d7107
Felipe Manga <lufeboma@gmail.com>
2022-01-09 15:39:06 +0000

    Initial filters support, implemented dropshadow filter

---
+++ src/filters/DropShadow.cpp
@@ -0,0 +1,73 @@
+// Copyright (c) 2021 LibreSprite Authors (cf. AUTHORS.md)
+// This file is released under the terms of the MIT license.
+// Read LICENSE.txt for more information.
+
+#include <common/Surface.hpp>
+#include <doc/Selection.hpp>
+#include <filters/Filter.hpp>
+
+class Surface;
+
+class DropShadown : public Filter {
+public:
+    Property<S32> offsetX{this, "offset-x", 0};
+    Property<S32> offsetY{this, "offset-y", 0};
+    Property<Color> shadowColor{this, "shadow-color", "rgba{0,0,0,255}"};
+    Property<U32> blur{this, "blur", 0};
+
+    virtual void run(std::shared_ptr<Surface> surface) {
+        S32 offsetX = this->offsetX;
+        S32 offsetY = this->offsetY;
+        if (offsetX == 0 && offsetY == 0)
+            return;
+
+        auto write = surface->data();
+        Vector<Surface::PixelType> src{write, surface->data() + surface->dataSize()};
+        auto width = surface->width();
+        auto height = surface->height();
+        U8 colorR = shadowColor->r;
+        U8 colorG = shadowColor->g;
+        U8 colorB = shadowColor->b;
+        U8 colorA = shadowColor->a;
+        if (!blur) {
+            U32 startY = 0;
+            U32 endY = height;
+            if (offsetY < 0) {
+                startY = -offsetY;
+                endY = height;
+            } else if (offsetY > 0) {
+                startY = 0;
+                endY = height - offsetY;
+            }
+
+            U32 startX = 0;
+            U32 endX = width;
+            if (offsetX < 0) {
+                startX = -offsetX;
+                endX = width;
+            } else if (offsetX > 0) {
+                startX = 0;
+                endX = width - offsetX;
+            }
+
+            for (U32 y = startY; y < endY; ++y) {
+                for (U32 x = startX; x < endX; ++x) {
+                    Color pixel{src[(y + offsetY) * width + (x + offsetX)]};
+                    Color previous{src[y * width + x]};
+                    F32 alpha = previous.a / 255.0f;
+                    U8 shadownAlpha = colorA * (pixel.a / 255.0f);
+                    write[y * width + x] = Color{
+                        U8(colorR * (1 - alpha) + previous.r * alpha),
+                        U8(colorG * (1 - alpha) + previous.g * alpha),
+                        U8(colorB * (1 - alpha) + previous.b * alpha),
+                        U8(shadownAlpha + alpha * (255 - shadownAlpha)),
+                    }.toU32();
+                }
+            }
+        } else {
+        }
+        surface->setDirty();
+    }
+};
+
+static Filter::Shared<DropShadown> reg{"dropshadow"};
---
+++ src/filters/Filter.hpp
@@ -0,0 +1,36 @@
+// Copyright (c) 2021 LibreSprite Authors (cf. AUTHORS.md)
+// This file is released under the terms of the MIT license.
+// Read LICENSE.txt for more information.
+
+#pragma once
+
+#include <common/Color.hpp>
+#include <common/PropertySet.hpp>
+#include <common/inject.hpp>
+
+class Surface;
+
+class Filter : public Injectable<Filter>, public Model, public std::enable_shared_from_this<Filter> {
+public:
+    static inline HashMap<String, std::shared_ptr<Filter>> instances;
+    static inline std::weak_ptr<Filter> active;
+
+    static void boot() {
+        for (auto& entry : Filter::getAllWithoutFlag("noauto")) {
+            entry.second->init(entry.first);
+        }
+    }
+
+    using Path = Vector<Point2D>;
+    Property<bool> enabled{this, "enabled", true};
+
+    virtual void init(const String& name) {
+        instances.insert({name, shared_from_this()});
+        load({
+                {"icon", "%skin/" + name + ".png"},
+                {"filter", name}
+            });
+    }
+
+    virtual void run(std::shared_ptr<Surface> surface) = 0;
+};
