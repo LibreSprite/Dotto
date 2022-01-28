824f9376b9181fd7661d3ba0652146afc5db9e6b
Felipe Manga <lufeboma@gmail.com>
2022-01-28 21:30:38 +0000

    Added ToggleUI command for toggling node visibility with a shortcut

---
+++ src/cmd/ToggleUI.cpp
@@ -0,0 +1,30 @@
+// Copyright (c) 2021 LibreSprite Authors (cf. AUTHORS.md)
+// This file is released under the terms of the MIT license.
+// Read LICENSE.txt for more information.
+
+#include <cmd/Command.hpp>
+#include <common/Color.hpp>
+#include <common/Messages.hpp>
+#include <common/PubSub.hpp>
+#include <gui/Node.hpp>
+#include <log/Log.hpp>
+#include <tools/Tool.hpp>
+
+class ToggleUI : public Command {
+    Property<String> id{this, "id"};
+    Property<String> property{this, "property", "visible"};
+
+public:
+    void run() override {
+        inject<ui::Node> root{"root"};
+        if (root) {
+            if (auto node = root->findChildById(id)) {
+                auto& ps = node->getPropertySet();
+                auto current = ps.get<bool>(property);
+                node->set(property, !current);
+            }
+        }
+    }
+};
+
+static Command::Shared<ToggleUI> cmd{"toggleui"};
