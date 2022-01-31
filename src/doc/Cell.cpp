ec4bffbad9859f7bc36bb5c1cd1c88840f472d41
Felipe Manga <lufeboma@gmail.com>
2022-01-31 13:16:05 +0000

    Add Alpha property to Cell

--- src/doc/Cell.cpp
+++ src/doc/Cell.cpp
@@ -2,7 +2,21 @@
 // This file is released under the terms of the MIT license.
 // Read LICENSE.txt for more information.
 
+#include <common/Messages.hpp>
+#include <common/PubSub.hpp>
 #include <doc/Cell.hpp>
+
+void Cell::modify(bool silent) {
+    if (!silent) {
+        PubSub<>::pub(msg::ModifyCell{shared_from_this()});
+    }
+}
+
+void Cell::setAlpha(F32 v, bool silent) {
+    auto old = alpha;
+    alpha = std::clamp(v, 0.0f, 1.0f);
+    modify(old == alpha || silent);
+}
 
 class BitmapCell  : public Cell {
 public:
