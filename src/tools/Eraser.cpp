f7a735380609a63cc768de1e5b5c1f33e14279db
Felipe Manga <lufeboma@gmail.com>
2022-02-01 20:26:04 +0000

    Remove code duplication between Pencil and Eraser tools

--- src/tools/Eraser.cpp
+++ src/tools/Eraser.cpp
@@ -2,21 +2,13 @@
 // This file is released under the terms of the MIT license.
 // Read LICENSE.txt for more information.
 
-#include <cmd/Command.hpp>
-#include <common/line.hpp>
-#include <common/Surface.hpp>
-#include <doc/Selection.hpp>
-#include <tools/Tool.hpp>
+#include <tools/Pencil.hpp>
 
 class Surface;
 
-class Eraser : public Tool {
+class Eraser : public Pencil {
 public:
-    std::shared_ptr<Selection> selection;
-
-    std::shared_ptr<Command> paint;
-
-    virtual void begin(Surface* surface, const Vector<Point2D>& points) {
+    void begin(Surface* surface, const Vector<Point2D>& points) override {
         selection = inject<Selection>{"new"};
         paint = inject<Command>{"paint"};
         selection->add(points.back().x, points.back().y, 255);
@@ -25,20 +17,6 @@ public:
                 {"mode", "erase"},
                 {"preview", true}
             });
-        paint->run();
-    }
-
-    virtual void update(Surface* surface, const Vector<Point2D>& points) {
-        auto& end = points[points.size() - 1];
-        auto& begin = points[points.size() - 2];
-        line(begin, end, [=](const Point2D& point){
-            selection->add(point.x, point.y, 255);
-        });
-        paint->run();
-    }
-
-    virtual void end(Surface* surface, const Vector<Point2D>& points) {
-        paint->load({{"preview", false}});
         paint->run();
     }
 };
---
+++ src/tools/Pencil.hpp
@@ -0,0 +1,45 @@
+// Copyright (c) 2021 LibreSprite Authors (cf. AUTHORS.md)
+// This file is released under the terms of the MIT license.
+// Read LICENSE.txt for more information.
+
+#pragma once
+
+#include <cmd/Command.hpp>
+#include <common/line.hpp>
+#include <common/Surface.hpp>
+#include <doc/Selection.hpp>
+#include <tools/Tool.hpp>
+
+class Surface;
+
+class Pencil : public Tool {
+public:
+    std::shared_ptr<Selection> selection;
+
+    std::shared_ptr<Command> paint;
+
+    virtual void begin(Surface* surface, const Vector<Point2D>& points) {
+        selection = inject<Selection>{"new"};
+        paint = inject<Command>{"paint"};
+        selection->add(points.back().x, points.back().y, 255);
+        paint->load({
+                {"selection", selection},
+                {"preview", true}
+            });
+        paint->run();
+    }
+
+    virtual void update(Surface* surface, const Vector<Point2D>& points) {
+        auto& end = points[points.size() - 1];
+        auto& begin = points[points.size() - 2];
+        line(begin, end, [=](const Point2D& point){
+            selection->add(point.x, point.y, 255);
+        });
+        paint->run();
+    }
+
+    virtual void end(Surface* surface, const Vector<Point2D>& points) {
+        paint->load({{"preview", false}});
+        paint->run();
+    }
+};
