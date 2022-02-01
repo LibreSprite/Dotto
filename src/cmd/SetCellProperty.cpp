f6dfcbbb336b8cf67bb511e9878b3d32dd358506
Felipe Manga <lufeboma@gmail.com>
2022-02-01 19:06:31 +0000

    Use a command to set cell properties

---
+++ src/cmd/SetCellProperty.cpp
@@ -0,0 +1,52 @@
+// Copyright (c) 2021 LibreSprite Authors (cf. AUTHORS.md)
+// This file is released under the terms of the MIT license.
+// Read LICENSE.txt for more information.
+
+#include <cmd/Command.hpp>
+#include <common/Messages.hpp>
+#include <common/PubSub.hpp>
+#include <doc/Document.hpp>
+#include <doc/Timeline.hpp>
+#include <doc/Cell.hpp>
+#include <gui/Node.hpp>
+#include <log/Log.hpp>
+#include <memory>
+
+class SetCellProperty : public Command {
+    inject<ui::Node> editor{"activeeditor"};
+    Property<std::shared_ptr<Cell>> cell{this, "cell"};
+    Property<String> property{this, "property"};
+    Property<Value> value{this, "value"};
+    Value prevValue;
+
+public:
+    void undo() override {
+        if (*property == "alpha")
+            (*cell)->setAlpha(prevValue, false);
+    }
+
+    void run() override {
+        if (!*cell)
+            *cell = inject<Cell>{"activecell"};
+        if (!*cell)
+            return;
+        if (*property == "alpha") {
+            prevValue = (*cell)->getAlpha();
+            *value = getPropertySet().get<F32>("value");
+            (*cell)->setAlpha(*value, false);
+        } else {
+            logE("Invalid SetCellProperty property [", *property, "]");
+            return;
+        }
+        if (prevValue == *value)
+            return;
+        auto prev = std::dynamic_pointer_cast<SetCellProperty>(doc()->getLastCommand());
+        if (prev && *prev->cell == *cell && *prev->property == *property) {
+            prev->set("value", *value);
+        } else {
+            commit();
+        }
+    }
+};
+
+static Command::Shared<SetCellProperty> cmd{"setcellproperty"};
--- src/gui/controllers/CellPropertyModifier.cpp
+++ src/gui/controllers/CellPropertyModifier.cpp
@@ -2,6 +2,7 @@
 // This file is released under the terms of the MIT license.
 // Read LICENSE.txt for more information.
 
+#include <cmd/Command.hpp>
 #include <common/Messages.hpp>
 #include <common/PubSub.hpp>
 #include <doc/Cell.hpp>
@@ -12,9 +13,10 @@
 
 class CellPropertyModifier : public ui::Controller {
 public:
-    PubSub<> pub{this};
+    PubSub<msg::ModifyCell> pub{this};
     Property<std::shared_ptr<Cell>> cell{this, "cell", nullptr, &CellPropertyModifier::setCell};
     Property<String> property{this, "property"};
+    Property<Value> value{this, "value"};
 
     void setCell() {
         if (property->empty() || !*cell)
@@ -27,16 +29,22 @@ public:
         node()->addEventListener<ui::Changed>(this);
     }
 
-    template<typename Type>
-    Type value() {
-        return node()->getPropertySet().get<Type>("value");
+    void on(msg::ModifyCell& event) {
+        if (event.cell == *cell)
+            setCell();
     }
 
     void eventHandler(const ui::Changed&) {
-        if (property->empty() || !*cell)
+        if (!*cell)
             return;
-        if (*property == "alpha") {
-            (*cell)->setAlpha(value<F32>(), false);
+        inject<Command> cmd{"setcellproperty"};
+        if (cmd) {
+            cmd->load({
+                    {"cell", *cell},
+                    {"property", *property},
+                    {"value", *value}
+                });
+            cmd->run();
         }
     }
 };
