58bd62e0b46851daefa7a58cd13dc73a39d2e308
Felipe Manga <lufeboma@gmail.com>
2022-02-01 12:16:52 +0000

    Make Hover-Reveal more customizable

--- src/gui/controllers/hover-reveal.cpp
+++ src/gui/controllers/hover-reveal.cpp
@@ -14,32 +14,38 @@
 class HoverReveal : public ui::Controller {
 public:
     PubSub<> pub{this};
+    Property<F32> speed{this, "speed", 0.05f};
+    Property<F32> minalpha{this, "min-alpha", 0.2f};
+    Property<F32> maxalpha{this, "max-alpha", 1.0f};
+
     std::optional<PubSub<msg::Tick>> tickpub;
     bool hover = false;
 
     void attach() override {
         node()->addEventListener<ui::MouseEnter, ui::MouseLeave>(this);
+        node()->set("alpha", *minalpha);
     }
 
-    void eventHandler(const ui::MouseEnter& event) {
+    void eventHandler(const ui::MouseEnter&) {
         hover = true;
         tickpub.emplace(this);
     }
 
-    void eventHandler(const ui::MouseLeave& event) {
+    void eventHandler(const ui::MouseLeave&) {
         hover = false;
         tickpub.emplace(this);
     }
 
     void on(msg::Tick&) {
         auto alpha = node()->getPropertySet().get<F32>("alpha");
-        auto delta = (hover ? 1.0f : 0.1f) - alpha;
-        if (std::abs(delta) > 0.001) {
-            alpha += delta * 0.03f;
-            node()->set("alpha", alpha);
-        } else {
+        auto target = hover ? *maxalpha : *minalpha;
+        auto delta = (target - alpha) * speed;
+        alpha += delta;
+        if (std::abs(delta) < 0.001) {
             tickpub.reset();
+            alpha = target;
         }
+        node()->set("alpha", alpha);
     }
 };
 
