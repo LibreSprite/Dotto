c22641914f25a17faec9845bd1b1600977698b7a
FManga <lufeboma@gmail.com>
2022-02-03 10:48:32 +0000

    Removed constexpr because of GCC8

--- src/common/Color.hpp
+++ src/common/Color.hpp
@@ -42,44 +42,9 @@ public:
             "}";
     }
 
-    void fromString(const String& color) {
-        if (color.empty())
-            return;
-        if (color[0] == '#') {
-            U32 rgba = 0xFF;
-            std::from_chars(color.c_str() + 1, color.c_str() + color.size(), rgba, 16);
-            if (color.size() == 7) {
-                rgba |= 0xFF000000;
-            } else if (color.size() == 4) {
-                rgba = 0xFF000000 |
-                    ((rgba & 0xF00) << 24) | ((rgba & 0xF00) << 16) |
-                    ((rgba & 0x0F0) << 8) | ((rgba & 0x0F0) << 4) |
-                    ((rgba & 0x00F) << 4) | (rgba & 0x00F);
-            }
-            fromABGR(rgba);
-            return;
-        }
-        if (color.back() == '}') {
-            auto parts = split(color, "{");
-            if (parts.size() != 2)
-                return;
-            auto keys = trim(tolower(parts[0]));
-            auto values = split(parts[1], ",");
-            for (U32 i = 0; i < keys.size() && i < values.size(); ++i) {
-                auto key = keys[i];
-                auto value = std::stoi(trim(values[i]));
-                if (key == 'r') r = value;
-                else if (key == 'g') g = value;
-                else if (key == 'b') b = value;
-                else if (key == 'a') a = value;
-                else continue;
-            }
-            return;
-        }
-        fromU32(std::atof(color.c_str()));
-    }
+    void fromString(const String& color);
 
-    constexpr Color& fromU32(U32 rgba) {
+    Color& fromU32(U32 rgba) {
         r = static_cast<U8>(rgba >> Rshift);
         g = static_cast<U8>(rgba >> Gshift);
         b = static_cast<U8>(rgba >> Bshift);
