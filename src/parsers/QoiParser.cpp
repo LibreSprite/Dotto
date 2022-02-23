67bc67a4c923cd45f3d38822ec0dfe2b6e03df83
FManga <lufeboma@gmail.com>
2022-02-23 11:17:57 +0000

    Add support for reading and writing QOI files

---
+++ src/parsers/QoiParser.cpp
@@ -0,0 +1,36 @@
+// Copyright (c) 2021 LibreSprite Authors (cf. AUTHORS.md)
+// This file is released under the terms of the MIT license.
+// Read LICENSE.txt for more information.
+
+#include <qoi/qoi.h>
+
+#include <common/Parser.hpp>
+#include <common/Surface.hpp>
+#include <log/Log.hpp>
+
+
+class QoiParser : public Parser {
+public:
+    Value parseFile(std::shared_ptr<File> file) override {
+        int size = file->size();
+        Vector<U8> data;
+        data.resize(size);
+        file->read(data.data(), size);
+
+        qoi_desc desc;
+        std::unique_ptr<void, void(*)(void*)> pixels(qoi_decode(data.data(), size, &desc, 4), free);
+        if (!pixels)
+            return nullptr;
+
+        auto surface = std::make_shared<Surface>();
+        surface->resize(desc.width, desc.height);
+        surface->setDirty();
+        auto out = surface->data();
+        for (U32 i = 0; i < desc.width * desc.height; ++i)
+            out[i] = reinterpret_cast<Surface::PixelType*>(pixels.get())[i];
+
+        return surface;
+    }
+};
+
+static Parser::Shared<QoiParser> qoi{"qoi", {"image"}};
---
+++ src/writers/QoiWriter.cpp
@@ -0,0 +1,26 @@
+// Copyright (c) 2021 LibreSprite Authors (cf. AUTHORS.md)
+// This file is released under the terms of the MIT license.
+// Read LICENSE.txt for more information.
+
+#include <qoi/qoi.h>
+
+#include <common/Surface.hpp>
+#include <common/Writer.hpp>
+#include <log/Log.hpp>
+
+class QoiWriter : public SimpleImageWriter {
+public:
+    bool writeFile(std::shared_ptr<File> file, const Value& data) override {
+        std::shared_ptr<Surface> surface = data;
+        qoi_desc desc;
+        desc.width = surface->width();
+        desc.height = surface->height();
+        desc.channels = 4;
+        desc.colorspace = QOI_SRGB;
+        int size = 0;
+        std::unique_ptr<void, void(*)(void*)> out(qoi_encode(surface->data(), &desc, &size), free);
+        return out && file->write(out.get(), size) == size;
+    }
+};
+
+static Writer::Shared<QoiWriter> qoi{"qoi", {"*.qoi"}};
