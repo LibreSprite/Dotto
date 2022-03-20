522a2c707efcb97b89f227293e6dfaad431890e9
Felipe Manga <lufeboma@gmail.com>
2022-03-20 22:05:36 +0000

    Initial Nintendo3DS support

---
+++ src/sys/n3ds/main.cpp
@@ -0,0 +1,25 @@
+// Copyright (c) 2021 LibreSprite Authors (cf. AUTHORS.md)
+// This file is released under the terms of the MIT license.
+// Read LICENSE.txt for more information.
+
+#ifdef __N3DS__
+
+#include <3ds.h>
+
+#include <app/App.hpp>
+#include <common/System.hpp>
+#include <log/Log.hpp>
+
+int main(int argc, const char* argv[]) {
+    osSetSpeedupEnable(true);
+    Log::setDefault("stdout");
+    inject<App> app{"dotto"};
+    app->boot(argc, argv);
+
+    Log::write(Log::Level::VERBOSE, "Running Dotto!");
+    while(app->run());
+
+    return 0;
+}
+
+#endif
