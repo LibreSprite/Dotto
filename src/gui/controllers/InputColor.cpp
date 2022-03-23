af054bbea82d5ed93107cfedf04ce697912e408c
Felipe Manga <lufeboma@gmail.com>
2022-03-23 23:56:08 +0000

    Revamp Color Input widget

--- src/gui/controllers/InputColor.cpp
+++ src/gui/controllers/InputColor.cpp
@@ -2,35 +2,66 @@
 // This file is released under the terms of the MIT license.
 // Read LICENSE.txt for more information.
 
+#include <cmd/Command.hpp>
 #include <common/Messages.hpp>
 #include <common/PubSub.hpp>
 #include <gui/Controller.hpp>
 #include <gui/Events.hpp>
 #include <gui/Node.hpp>
+#include <gui/Window.hpp>
+#include <log/Log.hpp>
 #include <tools/Tool.hpp>
 
 class InputColor : public ui::Controller {
     PubSub<msg::ActivateColor> pub{this};
-    std::shared_ptr<ui::Node> picker;
+    Property<String> pickerId{this, "picker-id", "colorpanel"};
+    Property<String> hide{this, "hide", ""};
     Property<bool> alwaysUpdate{this, "alwaysupdate", false};
+    InputColor* activeInput = nullptr;
 
 public:
+    ~InputColor() {
+        if (activeInput == this)
+            activeInput = nullptr;
+    }
+
     void attach() override {
         node()->addEventListener<ui::Click>(this);
-        if (alwaysUpdate)
-            node()->set("value", Tool::color.toString());
+        String color;
+        if (alwaysUpdate) {
+            color = Tool::color.toString();
+        } else {
+            color = node()->getPropertySet().get<Color>("value");
+        }
+
+        logI("Attach IC ", color);
+        node()->set("value", color);
     }
 
     void eventHandler(const ui::Click&) {
-        if (picker && !picker->getParent())
-            picker.reset();
-        if (!picker)
-            picker = ui::Node::fromXML("colorpicker");
-        picker->bringToFront();
+        String mode = "toggle";
+        if (activeInput != this) {
+            node()->getPropertySet().print();
+            inject<Command> activate{"activatecolor"};
+            activate->set("color", node()->getPropertySet().get<Color>("value"));
+            activate->run();
+            mode = "set";
+        }
+
+        activeInput = this;
+
+        if (auto toggleui = inject<Command>{"toggleui"}) {
+            toggleui->load({
+                    {"id", *pickerId},
+                    {"hide", *hide},
+                    {"mode", mode}
+                });
+            toggleui->run();
+        }
     }
 
     void on(msg::ActivateColor& event) {
-        if (picker || alwaysUpdate) {
+        if (alwaysUpdate || activeInput == this) {
             node()->set("value", event.color.toString());
             node()->processEvent(ui::Changed{node()});
         }
