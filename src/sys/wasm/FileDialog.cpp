e0cbe7a05976d734ce73afa88a2caa161c72297c
Felipe Manga <lufeboma@gmail.com>
2022-03-26 19:41:32 +0000

    Stub FileDialog in emscripten to prevent crash

---
+++ src/sys/wasm/FileDialog.cpp
@@ -0,0 +1,60 @@
+// Copyright (c) 2021 LibreSprite Authors (cf. AUTHORS.md)
+// This file is released under the terms of the MIT license.
+// Read LICENSE.txt for more information.
+
+#if defined(EMSCRIPTEN)
+
+#include <common/String.hpp>
+#include <fs/FileDialog.hpp>
+#include <log/Log.hpp>
+#include <task/TaskManager.hpp>
+
+class WasmFileDialog : public FileDialog {
+public:
+    TaskHandle handle;
+    inject<TaskManager> taskManager;
+
+    void open(Callback&& callback) override {
+        handle = taskManager->add(
+            [
+                filters = this->filters,
+                defaultPath = this->defaultPath,
+                filterDescription = this->filterDescription,
+                title = this->title,
+                allowMultiple = this->allowMultiple
+             ]()->Value{
+                return Vector<String>{};
+            },
+            [=](Value&& result){
+                if (result.has<Vector<String>>()) {
+                    callback(result.get<Vector<String>>());
+                } else {
+                    callback({});
+                }
+            });
+    }
+
+    void save(Callback&& callback) override {
+        handle = taskManager->add(
+            [
+                filters = this->filters,
+                defaultPath = this->defaultPath,
+                filterDescription = this->filterDescription,
+                title = this->title,
+                allowMultiple = this->allowMultiple
+             ]()->Value{
+                return String{};
+            },
+            [=](Value&& result){
+                if (result.has<String>()) {
+                    callback(Vector<String>{result.get<String>()});
+                } else {
+                    callback({});
+                }
+            });
+    }
+};
+
+static FileDialog::Singleton<WasmFileDialog> reg{""};
+
+#endif
