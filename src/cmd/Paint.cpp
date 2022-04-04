1f981a7c53aa576954bdb2f347f2b4bc47f7ad20
Felipe Manga <lufeboma@gmail.com>
2022-04-04 20:18:23 +0100

    Add blend mode support to Paint command

--- src/cmd/Paint.cpp
+++ src/cmd/Paint.cpp
@@ -2,6 +2,7 @@
 // This file is released under the terms of the MIT license.
 // Read LICENSE.txt for more information.
 
+#include <blender/Blender.hpp>
 #include <cmd/Command.hpp>
 #include <common/Messages.hpp>
 #include <common/PubSub.hpp>
@@ -20,7 +21,7 @@ class Paint : public Command {
     Property<Color> color{this, "color", Tool::color.toString()};
     Property<bool> preview{this, "preview", false};
     Property<bool> cursor{this, "cursor", false};
-    Property<String> mode{this, "mode", "simple"};
+    Property<String> mode{this, "mode", "normal"};
     Vector<U32> undoData;
 
 public:
@@ -128,12 +129,11 @@ public:
         if (maskRect.empty())
             return;
 
-        F32 alpha = color.a / 255.0f / 255.0f;
         auto& mask = selection->getData();
         auto commonRect = maskRect;
         auto maskStride = maskRect.width;
         auto surfaceStride = surface->width();
-        Color tmp;
+
         commonRect.intersect({0, 0, surface->width(), surface->height()});
 
         U32 maskOffsetY = commonRect.y > 0 ? 0 : -commonRect.y;
@@ -141,32 +141,16 @@ public:
         U32 surfaceOffsetY = commonRect.y > 0 ? commonRect.y : 0;
         U32 surfaceOffsetX = commonRect.x > 0 ? commonRect.x : 0;
 
-        if (*mode == "simple") {
+        inject<Blender> blender{*mode};
+        if (blender) {
+            Color low;
             for (U32 y = 0; y < commonRect.height; ++y) {
                 U32 maskIndex = (y + maskOffsetY) * maskStride + maskOffsetX;
                 U32 surfaceIndex = (y + surfaceOffsetY) * surfaceStride + surfaceOffsetX;
                 for (U32 x = 0; x < commonRect.width; ++x, ++maskIndex, ++surfaceIndex) {
-                    auto amount = mask[maskIndex] * alpha;
-                    tmp.fromU32(readData[surfaceIndex]);
-                    tmp.r = tmp.r * (1 - amount) + color.r * amount;
-                    tmp.g = tmp.g * (1 - amount) + color.g * amount;
-                    tmp.b = tmp.b * (1 - amount) + color.b * amount;
-                    tmp.a = tmp.a + amount * (255 - tmp.a);
-                    writeData[surfaceIndex] = tmp.toU32();
-                }
-            }
-        } else if (*mode == "erase") {
-            for (U32 y = 0; y < commonRect.height; ++y) {
-                U32 maskIndex = (y + maskOffsetY) * maskStride + maskOffsetX;
-                U32 surfaceIndex = (y + surfaceOffsetY) * surfaceStride + surfaceOffsetX;
-                for (U32 x = 0; x < commonRect.width; ++x, ++maskIndex, ++surfaceIndex) {
-                    auto amount = mask[maskIndex] * alpha;
-                    tmp.fromU32(readData[surfaceIndex]);
-                    tmp.r = tmp.r * (1 - amount);
-                    tmp.g = tmp.g * (1 - amount);
-                    tmp.b = tmp.b * (1 - amount);
-                    tmp.a = tmp.a * (1 - amount);
-                    writeData[surfaceIndex] = tmp.toU32();
+                    auto amount = mask[maskIndex] * (1.0f / 255.0f);
+                    low.fromU32(readData[surfaceIndex]);
+                    writeData[surfaceIndex] = blender->blendPixel(low, color, amount);
                 }
             }
         }
