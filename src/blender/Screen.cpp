d327c3af282ea361aa492f804fe4f659409540e2
Felipe Manga <lufeboma@gmail.com>
2022-04-04 20:14:24 +0100

    Reimplement Screen blend mode

--- src/blender/Screen.cpp
+++ src/blender/Screen.cpp
@@ -8,10 +8,10 @@ class Screen : public Blender {
 public:
     Surface::PixelType blendPixel(const Color& a, const Color& b, F32 alpha) override {
         alpha *= b.a * (1.0f / 255.0f);
-        color.r = std::min<U32>(255, F32(a.r) + b.r * alpha);
-        color.g = std::min<U32>(255, F32(a.g) + b.g * alpha);
-        color.b = std::min<U32>(255, F32(a.b) + b.b * alpha);
-        color.a = a.a + alpha * (255 - a.a);
+        color.r = a.r * (1 - alpha) + (255 - ((255 - a.r) * (255 - b.r) >> 8)) * alpha + 0.5f;
+        color.g = a.g * (1 - alpha) + (255 - ((255 - a.g) * (255 - b.g) >> 8)) * alpha + 0.5f;
+        color.b = a.b * (1 - alpha) + (255 - ((255 - a.b) * (255 - b.b) >> 8)) * alpha + 0.5f;
+        color.a = a.a + alpha * (255 - a.a) + 0.5f;
         return color.toU32();
     }
 };
