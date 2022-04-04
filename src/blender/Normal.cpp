8faa082660db56b20cd4edc7c0b92530f9f9b600
Felipe Manga <lufeboma@gmail.com>
2022-04-04 20:13:51 +0100

    Use rounding when blending in Normal mode

--- src/blender/Normal.cpp
+++ src/blender/Normal.cpp
@@ -8,10 +8,10 @@ class Normal : public Blender {
 public:
     Surface::PixelType blendPixel(const Color& a, const Color& b, F32 alpha) override {
         alpha *= b.a * (1.0f / 255.0f);
-        color.r = a.r * (1 - alpha) + b.r * alpha;
-        color.g = a.g * (1 - alpha) + b.g * alpha;
-        color.b = a.b * (1 - alpha) + b.b * alpha;
-        color.a = a.a + alpha * (255 - a.a);
+        color.r = a.r * (1 - alpha) + b.r * alpha + 0.5f;
+        color.g = a.g * (1 - alpha) + b.g * alpha + 0.5f;
+        color.b = a.b * (1 - alpha) + b.b * alpha + 0.5f;
+        color.a = a.a + alpha * (255 - a.a) + 0.5f;
         return color.toU32();
     }
 };
