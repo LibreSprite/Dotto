85ef599593df4e51a08a821f88ba02b9eb1cc281
Felipe Manga <lufeboma@gmail.com>
2022-04-13 18:08:46 +0100

    Implement Android-specific main

---
+++ src/sys/android/main.cpp
@@ -0,0 +1,25 @@
+// Copyright (c) 2021 LibreSprite Authors (cf. AUTHORS.md)
+// This file is released under the terms of the MIT license.
+// Read LICENSE.txt for more information.
+
+#if defined(ANDROID)
+#include <android/log.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <app/App.hpp>
+#include <common/System.hpp>
+#include <log/Log.hpp>
+
+extern "C" __attribute__((visibility("default"))) int SDL_main(int argc, const char* argv[]) {
+    Log::setDefault("stdout");
+    inject<App> app{"dotto"};
+    app->boot(argc, argv);
+    logI("Running Dotto!");
+    while(app->run());
+    return 0;
+}
+
+#endif
