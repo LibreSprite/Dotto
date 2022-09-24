1764ee6215be6dff7d9b73f0efe899737d0595db
FManga <lufeboma@gmail.com>
2022-09-24 09:17:54 +0200

    Allow scripts to alter setting values

--- src/script/api/ConfigScriptObject.cpp
+++ src/script/api/ConfigScriptObject.cpp
@@ -28,6 +28,31 @@ public:
             return nullptr;
         });
 
+        addFunction("set", [=](const String& name, const script::Value& value) {
+            std::shared_ptr<PropertySet> properties = config->properties;
+            auto parts = split(name, ".");
+            for (std::size_t i = 0, max = parts.size(); i < max; ++i) {
+                if (!properties)
+                    break;
+                if (i == max - 1) {
+                    properties->set(parts[i], value.get());
+                    config->dirty();
+                    logI("Set ", parts[i], value);
+                    return true;
+                }
+                auto& map = properties->getMap();
+                auto value = map.find(parts[i]);
+                if (value == map.end() || !value->second) {
+                    auto child = std::make_shared<PropertySet>();
+                    properties->set(parts[i], child);
+                    properties = child;
+                } else {
+                    properties = value->second->get<std::shared_ptr<PropertySet>>();
+                }
+            }
+            return false;
+        });
+
         makeGlobal("config");
     }
 };
