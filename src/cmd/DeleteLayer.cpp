ca8dd23e536ff15a60f2681053d3416e562143cb
FManga <lufeboma@gmail.com>
2022-10-05 01:06:25 +0200

    Allow deleting multiple layers at once

--- src/cmd/DeleteLayer.cpp
+++ src/cmd/DeleteLayer.cpp
@@ -5,58 +5,118 @@
 #include <cmd/Command.hpp>
 #include <common/Messages.hpp>
 #include <common/PubSub.hpp>
-#include <doc/Document.hpp>
-#include <doc/Timeline.hpp>
 #include <doc/Cell.hpp>
+#include <doc/Document.hpp>
+#include <doc/GroupCell.hpp>
 #include <gui/Node.hpp>
 #include <log/Log.hpp>
+#include <memory>
 
 class DeleteLayer : public Command {
     inject<ui::Node> editor{"activeeditor"};
-    Property<String> cellType{this, "type", "bitmap"};
-    S32 layer, frame, next;
-    std::shared_ptr<Cell> cell;
+
+    struct Entry {
+        std::shared_ptr<Cell> cell;
+        std::shared_ptr<GroupCell> group;
+        bool isActive;
+        S32 layer;
+    };
+    Vector<Entry> cells;
+    S32 next = 0;
 
 public:
     void undo() override {
-        auto timeline = doc()->currentTimeline();
-        if (!timeline)
-            return;
-        timeline->setCell(frame, layer, cell);
-        editor->set("layer", layer);
+        for (auto& entry : cells) {
+            entry.group->setCell(entry.layer, entry.cell);
+            if (entry.isActive)
+                editor->set("layer", entry.layer);
+        }
+    }
+
+    bool willDelete(std::shared_ptr<Cell> cell) {
+        for (auto& entity : cells) {
+            if (entity.cell == cell)
+                return true;
+        }
+        return false;
+    }
+
+    std::shared_ptr<Cell> findNextActive(std::shared_ptr<GroupCell> group) {
+        std::shared_ptr<Cell> sibling, prevSibling, nextSibling;
+        bool foundDel = false;
+        for (U32 i = 0, max = group->layerCount(); i < max; ++i) {
+            auto cell = group->getCell(i);
+            if (!cell)
+                continue;
+            if (willDelete(cell)) {
+                if (!prevSibling) {
+                    prevSibling = sibling;
+                }
+                foundDel = true;
+            } else if (auto childGroup = std::dynamic_pointer_cast<GroupCell>(cell)) {
+                sibling = findNextActive(childGroup);
+                if (sibling && foundDel)
+                    return sibling;
+            } else if (foundDel) {
+                return cell;
+            } else {
+                sibling = cell;
+            }
+        }
+        return nextSibling ?: prevSibling;
     }
 
     void run() override {
         if (!doc())
             return;
-        auto timeline = doc()->currentTimeline();
-        if (!timeline)
-            return;
 
-        if (!cell) {
-            auto& ps = editor->getPropertySet();
-            frame = ps.get<S32>("frame");
-            layer = ps.get<S32>("layer");
+        if (cells.empty()) {
+            auto selected = PubSub<>::pub(msg::PollSelectedCells{doc()}).cells;
+            auto active = cell();
+            bool willDeleteActive = false;
 
-            S32 layerCount = timeline->layerCount();
-            next = layer - 1;
-            while (next > 0 && !timeline->getCell(frame, next))
-                next--;
+            for (auto cell : selected) {
+                auto parent = cell.get();
+                while (parent) {
+                    if (parent == active.get()) {
+                        willDeleteActive = true;
+                        goto endSearch;
+                    }
+                    parent = parent->parent();
+                }
+            }
+            endSearch:;
 
-            if (next < 0 || !timeline->getCell(frame, next)) {
-                next = layer + 1;
-                for (;next < layerCount && !timeline->getCell(frame, next); ++next);
+            for (auto cell : selected) {
+                auto group = std::dynamic_pointer_cast<GroupCell>(cell->parent()->shared_from_this());
+                if (!group)
+                    continue;
+                auto layer = group->getCellIndex(cell.get());
+                if (layer == -1) {
+                    logE("Cell isn't parent's child");
+                    continue;
+                }
+                cells.push_back({
+                        cell,
+                        group,
+                        cell == active,
+                        layer
+                    });
             }
 
-            if (!timeline->getCell(frame, next))
-                return;
-
-            cell = timeline->getCell(frame, layer);
-            if (!cell)
-                return;
+            if (willDeleteActive) {
+                auto timeline = doc()->currentTimeline();
+                auto nextActive = findNextActive(std::dynamic_pointer_cast<GroupCell>(timeline->getCell(timeline->frame())));
+                if (!nextActive) {
+                    logE("Can't delete last layer");
+                    return;
+                }
+                next = dynamic_cast<GroupCell*>(nextActive->parent())->getCellIndex(nextActive.get());
+            }
         }
-
-        timeline->setCell(frame, layer, nullptr);
+        for (auto& entry : cells) {
+            entry.group->setCell(entry.layer, nullptr);
+        }
         editor->set("layer", next);
         commit();
     }
