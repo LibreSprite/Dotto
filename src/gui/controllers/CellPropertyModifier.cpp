612d8f0e4a7896c37d59e843155a673c73c16d25
FManga <lufeboma@gmail.com>
2022-10-15 19:45:29 +0100

    Allow making cellpropertymodifiers for whichever is the active cell

--- src/gui/controllers/CellPropertyModifier.cpp
+++ src/gui/controllers/CellPropertyModifier.cpp
@@ -14,11 +14,15 @@
 
 class CellPropertyModifier : public ui::Controller {
 public:
-    PubSub<msg::ModifyCell> pub{this};
+    PubSub<msg::ModifyCell,
+           msg::ActivateCell,
+           msg::ActivateLayer> pub{this};
     Property<std::shared_ptr<Cell>> cell{this, "cell", nullptr, &CellPropertyModifier::setCell};
     Property<String> property{this, "property", "", &CellPropertyModifier::setOptions};
     Property<Value> value{this, "value"};
     Property<String> key{this, "key", "value", &CellPropertyModifier::setCell};
+
+    Cell* currentCell = nullptr;
 
     void setOptions() {
         if (*property == "blendmode") {
@@ -30,33 +34,56 @@ public:
         }
     }
 
+    std::shared_ptr<Cell> getCell() {
+        return *this->cell ?: inject<Cell>{"activecell"}.shared();
+    }
+
     void setCell() {
-        if (property->empty() || !*cell)
+        auto cell = getCell();
+        currentCell = cell.get();
+
+        if (property->empty() || !cell) {
+            node()->set("visible", false);
             return;
+        }
+
+        node()->set("visible", true);
+
         if (*property == "alpha")
-            node()->set(*key, tostring((*cell)->getAlpha()));
+            node()->set(*key, tostring(cell->getAlpha()));
         else if (*property == "blendmode")
-            node()->set(*key, (*cell)->getBlendMode());
+            node()->set(*key, cell->getBlendMode());
         else if (*property == "name")
-            node()->set(*key, (*cell)->getName());
+            node()->set(*key, cell->getName());
     }
 
     void attach() override {
         node()->addEventListener<ui::Changed>(this);
     }
 
+    void on(msg::ActivateCell&) {
+        if (!*cell)
+            setCell();
+    }
+
+    void on(msg::ActivateLayer&) {
+        if (!*cell)
+            setCell();
+    }
+
     void on(msg::ModifyCell& event) {
-        if (event.cell == *cell)
+        if (event.cell.get() == currentCell)
             setCell();
     }
 
     void eventHandler(const ui::Changed&) {
-        if (!*cell)
+        auto cell = getCell();
+        if (!cell)
             return;
-        inject<Command> cmd{"setcellproperty"};
-        if (cmd) {
+
+        if (auto cmd = inject<Command>{"setcellproperty"}) {
             cmd->load({
-                    {"cell", *cell},
+                    {"cell", cell},
                     {"property", *property},
                     {"value", *value}
                 });
